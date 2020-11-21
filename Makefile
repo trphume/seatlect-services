e04e9df6301d2893703e5667cc7617ce90e30770
Traphume <traphumedev@gmail.com>
2020-11-21 17:10:20 +0700

    Refactor auth api to be user api

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
-.PHONY: gen_proto gen_proto_common gen_proto_entity clean_proto
+.PHONY: gen_proto gen_proto_common gen_proto_user clean_proto
 
-gen_proto: gen_proto_common gen_proto_auth
+gen_proto: gen_proto_common gen_proto_user
 
 gen_proto_common:
 	@protoc --go_out=. -I=. api/protobuf/common.proto
 
-gen_proto_auth:
-	@protoc --go_out=. --go-grpc_out=. -I=. api/protobuf/auth.proto
+gen_proto_user:
+	@protoc --go_out=. --go-grpc_out=. -I=. api/protobuf/user.proto
 
 clean_proto:
 	@rm -rf internal/genproto/
--- internal/genproto/userpb/user.pb.go
+++ internal/genproto/userpb/user.pb.go
@@ -2,9 +2,9 @@
 // versions:
 // 	protoc-gen-go v1.25.0-devel
 // 	protoc        v3.13.0
-// source: api/protobuf/auth.proto
+// source: api/protobuf/user.proto
 
-package authpb
+package userpb
 
 import (
 	timestamp "github.com/golang/protobuf/ptypes/timestamp"
@@ -35,7 +35,7 @@ type SignInRequest struct {
 func (x *SignInRequest) Reset() {
 	*x = SignInRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_api_protobuf_auth_proto_msgTypes[0]
+		mi := &file_api_protobuf_user_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -48,7 +48,7 @@ func (x *SignInRequest) String() string {
 func (*SignInRequest) ProtoMessage() {}
 
 func (x *SignInRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_api_protobuf_auth_proto_msgTypes[0]
+	mi := &file_api_protobuf_user_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -61,7 +61,7 @@ func (x *SignInRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use SignInRequest.ProtoReflect.Descriptor instead.
 func (*SignInRequest) Descriptor() ([]byte, []int) {
-	return file_api_protobuf_auth_proto_rawDescGZIP(), []int{0}
+	return file_api_protobuf_user_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *SignInRequest) GetUsername() string {
@@ -91,7 +91,7 @@ type SignInResponse struct {
 func (x *SignInResponse) Reset() {
 	*x = SignInResponse{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_api_protobuf_auth_proto_msgTypes[1]
+		mi := &file_api_protobuf_user_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -104,7 +104,7 @@ func (x *SignInResponse) String() string {
 func (*SignInResponse) ProtoMessage() {}
 
 func (x *SignInResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_api_protobuf_auth_proto_msgTypes[1]
+	mi := &file_api_protobuf_user_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -117,7 +117,7 @@ func (x *SignInResponse) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use SignInResponse.ProtoReflect.Descriptor instead.
 func (*SignInResponse) Descriptor() ([]byte, []int) {
-	return file_api_protobuf_auth_proto_rawDescGZIP(), []int{1}
+	return file_api_protobuf_user_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *SignInResponse) GetRefreshToken() string {
@@ -155,7 +155,7 @@ type SignUpRequest struct {
 func (x *SignUpRequest) Reset() {
 	*x = SignUpRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_api_protobuf_auth_proto_msgTypes[2]
+		mi := &file_api_protobuf_user_proto_msgTypes[2]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -168,7 +168,7 @@ func (x *SignUpRequest) String() string {
 func (*SignUpRequest) ProtoMessage() {}
 
 func (x *SignUpRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_api_protobuf_auth_proto_msgTypes[2]
+	mi := &file_api_protobuf_user_proto_msgTypes[2]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -181,7 +181,7 @@ func (x *SignUpRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use SignUpRequest.ProtoReflect.Descriptor instead.
 func (*SignUpRequest) Descriptor() ([]byte, []int) {
-	return file_api_protobuf_auth_proto_rawDescGZIP(), []int{2}
+	return file_api_protobuf_user_proto_rawDescGZIP(), []int{2}
 }
 
 func (x *SignUpRequest) GetUsername() string {
@@ -218,7 +218,7 @@ type SignUpResponse struct {
 func (x *SignUpResponse) Reset() {
 	*x = SignUpResponse{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_api_protobuf_auth_proto_msgTypes[3]
+		mi := &file_api_protobuf_user_proto_msgTypes[3]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -231,7 +231,7 @@ func (x *SignUpResponse) String() string {
 func (*SignUpResponse) ProtoMessage() {}
 
 func (x *SignUpResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_api_protobuf_auth_proto_msgTypes[3]
+	mi := &file_api_protobuf_user_proto_msgTypes[3]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -244,7 +244,7 @@ func (x *SignUpResponse) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use SignUpResponse.ProtoReflect.Descriptor instead.
 func (*SignUpResponse) Descriptor() ([]byte, []int) {
-	return file_api_protobuf_auth_proto_rawDescGZIP(), []int{3}
+	return file_api_protobuf_user_proto_rawDescGZIP(), []int{3}
 }
 
 func (x *SignUpResponse) GetRefreshToken() string {
@@ -278,7 +278,7 @@ type FetchJWTRequest struct {
 func (x *FetchJWTRequest) Reset() {
 	*x = FetchJWTRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_api_protobuf_auth_proto_msgTypes[4]
+		mi := &file_api_protobuf_user_proto_msgTypes[4]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -291,7 +291,7 @@ func (x *FetchJWTRequest) String() string {
 func (*FetchJWTRequest) ProtoMessage() {}
 
 func (x *FetchJWTRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_api_protobuf_auth_proto_msgTypes[4]
+	mi := &file_api_protobuf_user_proto_msgTypes[4]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -304,7 +304,7 @@ func (x *FetchJWTRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use FetchJWTRequest.ProtoReflect.Descriptor instead.
 func (*FetchJWTRequest) Descriptor() ([]byte, []int) {
-	return file_api_protobuf_auth_proto_rawDescGZIP(), []int{4}
+	return file_api_protobuf_user_proto_rawDescGZIP(), []int{4}
 }
 
 type FetchJWTResponse struct {
@@ -318,7 +318,7 @@ type FetchJWTResponse struct {
 func (x *FetchJWTResponse) Reset() {
 	*x = FetchJWTResponse{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_api_protobuf_auth_proto_msgTypes[5]
+		mi := &file_api_protobuf_user_proto_msgTypes[5]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -331,7 +331,7 @@ func (x *FetchJWTResponse) String() string {
 func (*FetchJWTResponse) ProtoMessage() {}
 
 func (x *FetchJWTResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_api_protobuf_auth_proto_msgTypes[5]
+	mi := &file_api_protobuf_user_proto_msgTypes[5]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -344,7 +344,7 @@ func (x *FetchJWTResponse) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use FetchJWTResponse.ProtoReflect.Descriptor instead.
 func (*FetchJWTResponse) Descriptor() ([]byte, []int) {
-	return file_api_protobuf_auth_proto_rawDescGZIP(), []int{5}
+	return file_api_protobuf_user_proto_rawDescGZIP(), []int{5}
 }
 
 func (x *FetchJWTResponse) GetJwtToken() string {
@@ -354,11 +354,11 @@ func (x *FetchJWTResponse) GetJwtToken() string {
 	return ""
 }
 
-var File_api_protobuf_auth_proto protoreflect.FileDescriptor
+var File_api_protobuf_user_proto protoreflect.FileDescriptor
 
-var file_api_protobuf_auth_proto_rawDesc = []byte{
-	0x0a, 0x17, 0x61, 0x70, 0x69, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f, 0x61,
-	0x75, 0x74, 0x68, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x08, 0x73, 0x65, 0x61, 0x74, 0x6c,
+var file_api_protobuf_user_proto_rawDesc = []byte{
+	0x0a, 0x17, 0x61, 0x70, 0x69, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f, 0x75,
+	0x73, 0x65, 0x72, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x08, 0x73, 0x65, 0x61, 0x74, 0x6c,
 	0x65, 0x63, 0x74, 0x1a, 0x1f, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2f, 0x70, 0x72, 0x6f, 0x74,
 	0x6f, 0x62, 0x75, 0x66, 0x2f, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x2e, 0x70,
 	0x72, 0x6f, 0x74, 0x6f, 0x1a, 0x19, 0x61, 0x70, 0x69, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62,
@@ -394,7 +394,7 @@ var file_api_protobuf_auth_proto_rawDesc = []byte{
 	0x0a, 0x10, 0x46, 0x65, 0x74, 0x63, 0x68, 0x4a, 0x57, 0x54, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e,
 	0x73, 0x65, 0x12, 0x1a, 0x0a, 0x08, 0x6a, 0x77, 0x74, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x18, 0x01,
 	0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x6a, 0x77, 0x74, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x32, 0xca,
-	0x01, 0x0a, 0x0b, 0x41, 0x75, 0x74, 0x68, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x3b,
+	0x01, 0x0a, 0x0b, 0x55, 0x73, 0x65, 0x72, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x3b,
 	0x0a, 0x06, 0x53, 0x69, 0x67, 0x6e, 0x49, 0x6e, 0x12, 0x17, 0x2e, 0x73, 0x65, 0x61, 0x74, 0x6c,
 	0x65, 0x63, 0x74, 0x2e, 0x53, 0x69, 0x67, 0x6e, 0x49, 0x6e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
 	0x74, 0x1a, 0x18, 0x2e, 0x73, 0x65, 0x61, 0x74, 0x6c, 0x65, 0x63, 0x74, 0x2e, 0x53, 0x69, 0x67,
@@ -408,23 +408,23 @@ var file_api_protobuf_auth_proto_rawDesc = []byte{
 	0x1a, 0x2e, 0x73, 0x65, 0x61, 0x74, 0x6c, 0x65, 0x63, 0x74, 0x2e, 0x46, 0x65, 0x74, 0x63, 0x68,
 	0x4a, 0x57, 0x54, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x42, 0x1a, 0x5a, 0x18, 0x69,
 	0x6e, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2f, 0x67, 0x65, 0x6e, 0x70, 0x72, 0x6f, 0x74, 0x6f,
-	0x2f, 0x61, 0x75, 0x74, 0x68, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x2f, 0x75, 0x73, 0x65, 0x72, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_api_protobuf_auth_proto_rawDescOnce sync.Once
-	file_api_protobuf_auth_proto_rawDescData = file_api_protobuf_auth_proto_rawDesc
+	file_api_protobuf_user_proto_rawDescOnce sync.Once
+	file_api_protobuf_user_proto_rawDescData = file_api_protobuf_user_proto_rawDesc
 )
 
-func file_api_protobuf_auth_proto_rawDescGZIP() []byte {
-	file_api_protobuf_auth_proto_rawDescOnce.Do(func() {
-		file_api_protobuf_auth_proto_rawDescData = protoimpl.X.CompressGZIP(file_api_protobuf_auth_proto_rawDescData)
+func file_api_protobuf_user_proto_rawDescGZIP() []byte {
+	file_api_protobuf_user_proto_rawDescOnce.Do(func() {
+		file_api_protobuf_user_proto_rawDescData = protoimpl.X.CompressGZIP(file_api_protobuf_user_proto_rawDescData)
 	})
-	return file_api_protobuf_auth_proto_rawDescData
+	return file_api_protobuf_user_proto_rawDescData
 }
 
-var file_api_protobuf_auth_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
-var file_api_protobuf_auth_proto_goTypes = []interface{}{
+var file_api_protobuf_user_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
+var file_api_protobuf_user_proto_goTypes = []interface{}{
 	(*SignInRequest)(nil),       // 0: seatlect.SignInRequest
 	(*SignInResponse)(nil),      // 1: seatlect.SignInResponse
 	(*SignUpRequest)(nil),       // 2: seatlect.SignUpRequest
@@ -434,16 +434,16 @@ var file_api_protobuf_auth_proto_goTypes = []interface{}{
 	(*commonpb.User)(nil),       // 6: seatlect.User
 	(*timestamp.Timestamp)(nil), // 7: google.protobuf.Timestamp
 }
-var file_api_protobuf_auth_proto_depIdxs = []int32{
+var file_api_protobuf_user_proto_depIdxs = []int32{
 	6, // 0: seatlect.SignInResponse.user:type_name -> seatlect.User
 	7, // 1: seatlect.SignUpRequest.dob:type_name -> google.protobuf.Timestamp
 	6, // 2: seatlect.SignUpResponse.user:type_name -> seatlect.User
-	0, // 3: seatlect.AuthService.SignIn:input_type -> seatlect.SignInRequest
-	2, // 4: seatlect.AuthService.SignUp:input_type -> seatlect.SignUpRequest
-	4, // 5: seatlect.AuthService.FetchJWT:input_type -> seatlect.FetchJWTRequest
-	1, // 6: seatlect.AuthService.SignIn:output_type -> seatlect.SignInResponse
-	3, // 7: seatlect.AuthService.SignUp:output_type -> seatlect.SignUpResponse
-	5, // 8: seatlect.AuthService.FetchJWT:output_type -> seatlect.FetchJWTResponse
+	0, // 3: seatlect.UserService.SignIn:input_type -> seatlect.SignInRequest
+	2, // 4: seatlect.UserService.SignUp:input_type -> seatlect.SignUpRequest
+	4, // 5: seatlect.UserService.FetchJWT:input_type -> seatlect.FetchJWTRequest
+	1, // 6: seatlect.UserService.SignIn:output_type -> seatlect.SignInResponse
+	3, // 7: seatlect.UserService.SignUp:output_type -> seatlect.SignUpResponse
+	5, // 8: seatlect.UserService.FetchJWT:output_type -> seatlect.FetchJWTResponse
 	6, // [6:9] is the sub-list for method output_type
 	3, // [3:6] is the sub-list for method input_type
 	3, // [3:3] is the sub-list for extension type_name
@@ -451,13 +451,13 @@ var file_api_protobuf_auth_proto_depIdxs = []int32{
 	0, // [0:3] is the sub-list for field type_name
 }
 
-func init() { file_api_protobuf_auth_proto_init() }
-func file_api_protobuf_auth_proto_init() {
-	if File_api_protobuf_auth_proto != nil {
+func init() { file_api_protobuf_user_proto_init() }
+func file_api_protobuf_user_proto_init() {
+	if File_api_protobuf_user_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_api_protobuf_auth_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_api_protobuf_user_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*SignInRequest); i {
 			case 0:
 				return &v.state
@@ -469,7 +469,7 @@ func file_api_protobuf_auth_proto_init() {
 				return nil
 			}
 		}
-		file_api_protobuf_auth_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_api_protobuf_user_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*SignInResponse); i {
 			case 0:
 				return &v.state
@@ -481,7 +481,7 @@ func file_api_protobuf_auth_proto_init() {
 				return nil
 			}
 		}
-		file_api_protobuf_auth_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+		file_api_protobuf_user_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*SignUpRequest); i {
 			case 0:
 				return &v.state
@@ -493,7 +493,7 @@ func file_api_protobuf_auth_proto_init() {
 				return nil
 			}
 		}
-		file_api_protobuf_auth_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+		file_api_protobuf_user_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*SignUpResponse); i {
 			case 0:
 				return &v.state
@@ -505,7 +505,7 @@ func file_api_protobuf_auth_proto_init() {
 				return nil
 			}
 		}
-		file_api_protobuf_auth_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
+		file_api_protobuf_user_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*FetchJWTRequest); i {
 			case 0:
 				return &v.state
@@ -517,7 +517,7 @@ func file_api_protobuf_auth_proto_init() {
 				return nil
 			}
 		}
-		file_api_protobuf_auth_proto_msgTypes[5].Exporter = func(v interface{}, i int) interface{} {
+		file_api_protobuf_user_proto_msgTypes[5].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*FetchJWTResponse); i {
 			case 0:
 				return &v.state
@@ -534,18 +534,18 @@ func file_api_protobuf_auth_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_api_protobuf_auth_proto_rawDesc,
+			RawDescriptor: file_api_protobuf_user_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   6,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
-		GoTypes:           file_api_protobuf_auth_proto_goTypes,
-		DependencyIndexes: file_api_protobuf_auth_proto_depIdxs,
-		MessageInfos:      file_api_protobuf_auth_proto_msgTypes,
+		GoTypes:           file_api_protobuf_user_proto_goTypes,
+		DependencyIndexes: file_api_protobuf_user_proto_depIdxs,
+		MessageInfos:      file_api_protobuf_user_proto_msgTypes,
 	}.Build()
-	File_api_protobuf_auth_proto = out.File
-	file_api_protobuf_auth_proto_rawDesc = nil
-	file_api_protobuf_auth_proto_goTypes = nil
-	file_api_protobuf_auth_proto_depIdxs = nil
+	File_api_protobuf_user_proto = out.File
+	file_api_protobuf_user_proto_rawDesc = nil
+	file_api_protobuf_user_proto_goTypes = nil
+	file_api_protobuf_user_proto_depIdxs = nil
 }
