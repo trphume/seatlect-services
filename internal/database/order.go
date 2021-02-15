f9616cb7c9c73d33a49eea811f65174cd90aeb93
Traphume <traphumedev@gmail.com>
2021-02-15 11:34:37 +0700

    Added order type to database pkg

---
+++ internal/database/order.go
@@ -0,0 +1,16 @@
+package database
+
+import "time"
+
+type Order struct {
+	Id            string    `bson:"_id"`
+	ReservationId string    `bson:"reservationId"`
+	CustomerId    string    `bson:"customerId"`
+	BusinessId    string    `bson:"businessId"`
+	Start         time.Time `bson:"start"`
+	End           time.Time `bson:"end"`
+	Seats         []Seat    `bson:"seats"`
+	Status        string    `bson:"status"`
+	Image         string    `bson:"image"`
+	ExtraSpace    int       `bson:"extraSpace"`
+}
