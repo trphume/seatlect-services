45af0c4be3dbb87c413d18763aeb6bca80c60241
Traphume <traphumedev@gmail.com>
2021-02-15 10:33:13 +0700

    Added common mongo customer type to database pkg

---
+++ internal/database/customer.go
@@ -0,0 +1,12 @@
+package database
+
+import "time"
+
+type Customer struct {
+	Id string `bson:"_id"`
+	Username string `bson:"username"`
+	Email string `bson:"email"`
+	Dob time.Time `bson:"dob"`
+	Favorite []string `bson:"favorite"`
+	Verified bool `bson:"verified"`
+}
