42e43688f816bed65dfb479adfef4c52daffa528
Traphume <traphumedev@gmail.com>
2021-02-16 16:51:09 +0700

    Added repo interface to communicate with mongodb for usermb pkg

--- internal/mobile/usermb/usermb.go
+++ internal/mobile/usermb/usermb.go
@@ -2,6 +2,7 @@ package usermb
 
 import (
 	"context"
+	"github.com/tphume/seatlect-services/internal/database/typedb"
 	"github.com/tphume/seatlect-services/internal/genproto/userpb"
 )
 
@@ -27,4 +28,17 @@ func (s *Server) AddFavorite(context.Context, *userpb.AddFavoriteRequest) (*user
 func (s *Server) RemoveFavorite(context.Context, *userpb.RemoveFavoriteRequest) (*userpb.RemoveFavoriteResponse, error) {
 	// TODO: Implement function
 	panic("To be implemented")
+}
+
+type Repo interface {
+	// customer is an out parameter - values will be overwritten
+	// will return a user token
+	AuthenticateCustomer(ctx *context.Context, customer *typedb.Customer, password string) (string, error)
+
+	// customer is an out parameter - values will be overwritten
+	// will return a user token
+	CreateCustomer(ctx *context.Context, customer *typedb.Customer, password string) (string, error)
+
+	AppendFavorite(ctx *context.Context, businessId string) error
+	RemoveFavorite(ctx *context.Context, businessId string) error
 }
