8a8e117069cf0178dcfd09f1f0b87af901edffb3
Traphume <traphumedev@gmail.com>
2021-02-17 14:23:51 +0700

    Added CustomerDB pkg

---
+++ internal/database/customerdb/Customerdb.go
@@ -0,0 +1,29 @@
+package customerdb
+
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+	"github.com/tphume/seatlect-services/internal/database/typedb"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type CustomerDB struct {
+	Mongo *mongo.Collection
+	Redis *redis.Client
+}
+
+func (c *CustomerDB) AuthenticateCustomer(ctx *context.Context, customer *typedb.Customer, password string) (string, error) {
+	panic("implement me")
+}
+
+func (c *CustomerDB) CreateCustomer(ctx *context.Context, customer *typedb.Customer, password string) (string, error) {
+	panic("implement me")
+}
+
+func (c *CustomerDB) AppendFavorite(ctx *context.Context, businessId string) error {
+	panic("implement me")
+}
+
+func (c *CustomerDB) RemoveFavorite(ctx *context.Context, businessId string) error {
+	panic("implement me")
+}
