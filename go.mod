6ab5aeac8d9b62b543cb7c446d3544653515448a
Traphume <traphumedev@gmail.com>
2021-02-19 21:29:03 +0700

    Implemented CustomerDB authentication. Added bcrypt pkg

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/google/go-cmp v0.5.4 // indirect
 	github.com/labstack/echo/v4 v4.1.17
 	go.mongodb.org/mongo-driver v1.4.6
+	golang.org/x/crypto v0.0.0-20210218145215-b8e89b74b9df
 	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb // indirect
 	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9 // indirect
 	golang.org/x/sys v0.0.0-20210112080510-489259a85091 // indirect
--- internal/database/customerdb/Customerdb.go
+++ internal/database/customerdb/Customerdb.go
@@ -2,19 +2,40 @@ package customerdb
 
 import (
 	"context"
+	"github.com/tphume/seatlect-services/internal/commonErr"
 	"github.com/tphume/seatlect-services/internal/database/typedb"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type CustomerDB struct {
 	Mongo *mongo.Collection
 }
 
-func (c *CustomerDB) AuthenticateCustomer(ctx context.Context, customer *typedb.Customer, password string) (string, error) {
-	panic("implement me")
+func (c *CustomerDB) AuthenticateCustomer(ctx context.Context, customer *typedb.Customer) (string, error) {
+	res := c.Mongo.FindOne(ctx, bson.M{"_id": customer.Id})
+	if res.Err() != nil {
+		if res.Err() == mongo.ErrNoDocuments {
+			return "", commonErr.NOTFOUND
+		}
+
+		return "", commonErr.INTERNAL
+	}
+
+	pw := customer.Password
+	if err := res.Decode(customer); err != nil {
+		return "", commonErr.INTERNAL
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(customer.Password), []byte(pw)); err != nil {
+		return "", commonErr.NOTFOUND
+	}
+
+	return customer.Id, nil
 }
 
-func (c *CustomerDB) CreateCustomer(ctx context.Context, customer *typedb.Customer, password string) (string, error) {
+func (c *CustomerDB) CreateCustomer(ctx context.Context, customer *typedb.Customer) (string, error) {
 	panic("implement me")
 }
 
