bf8c23333edcaf379c412a0e0160b628b16c0be2
Traphume <traphumedev@gmail.com>
2021-02-22 18:09:00 +0700

    Added placehodler ListBusiness businessdb repo

--- internal/database/businessdb/businessdb.go
+++ internal/database/businessdb/businessdb.go
@@ -7,14 +7,35 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type BusinessDB struct {
 	BusCol *mongo.Collection
 }
 
+// TODO: proper search functionality
 func (b *BusinessDB) ListBusiness(ctx context.Context, searchParams typedb.ListBusinessParams) ([]typedb.Business, error) {
-	panic("implement me")
+	// Construcut params
+	limit := new(int64)
+	*limit = int64(searchParams.Limit)
+
+	// TODO: construct sorting option
+
+	businesses, err := b.BusCol.Find(ctx, bson.M{}, &options.FindOptions{
+		Limit: limit,
+	})
+
+	if err != nil {
+		return nil, commonErr.INTERNAL
+	}
+
+	var res []typedb.Business
+	if err = businesses.All(ctx, &res); err != nil {
+		return nil, commonErr.INTERNAL
+	}
+
+	return res, nil
 }
 
 func (b *BusinessDB) ListBusinessByIds(ctx context.Context, ids []string) ([]typedb.Business, error) {
