4c10d90d946387f5f951983b0d0e69d2d46b110e
Traphume <traphumedev@gmail.com>
2021-02-22 13:52:49 +0700

    Implemented businessmb ListBusinessById. Change typedb.businessid to primitive object id

--- internal/database/typedb/business.go
+++ internal/database/typedb/business.go
@@ -1,25 +1,26 @@
 package typedb
 
 import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"time"
 )
 
 type Business struct {
-	Id           string      `bson:"_id"`
-	Username     string      `bson:"username"`
-	Email        string      `bson:"email"`
-	BusinessName string      `bson:"businessName"`
-	Type         string      `bson:"type"`
-	Tags         []string    `bson:"tags"`
-	Description  string      `bson:"description"`
-	Location     Location    `bson:"location"`
-	Address      string      `bson:"address"`
-	DisplayImage string      `bson:"displayImage"`
-	Images       []string    `bson:"images"`
-	Placement    Seat        `bson:"placement"`
-	Menu         []MenuItems `bson:"menu"`
-	Status       int         `bson:"status"`
-	Verified     bool        `bson:"verified"`
+	Id           primitive.ObjectID `bson:"_id"`
+	Username     string             `bson:"username"`
+	Email        string             `bson:"email"`
+	BusinessName string             `bson:"businessName"`
+	Type         string             `bson:"type"`
+	Tags         []string           `bson:"tags"`
+	Description  string             `bson:"description"`
+	Location     Location           `bson:"location"`
+	Address      string             `bson:"address"`
+	DisplayImage string             `bson:"displayImage"`
+	Images       []string           `bson:"images"`
+	Placement    Seat               `bson:"placement"`
+	Menu         []MenuItems        `bson:"menu"`
+	Status       int                `bson:"status"`
+	Verified     bool               `bson:"verified"`
 }
 
 type ListBusinessParams struct {
--- internal/mobile/businessmb/businessmb.go
+++ internal/mobile/businessmb/businessmb.go
@@ -4,9 +4,14 @@ import (
 	"context"
 	"github.com/tphume/seatlect-services/internal/database/typedb"
 	"github.com/tphume/seatlect-services/internal/genproto/businesspb"
+	"github.com/tphume/seatlect-services/internal/genproto/commonpb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Server struct {
+	repo Repo
+
 	businesspb.UnimplementedBusinessServiceServer
 }
 
@@ -14,11 +19,51 @@ func (s *Server) ListBusiness(ctx context.Context, request *businesspb.ListBusin
 	panic("implement me")
 }
 
-func (s *Server) ListBusinessById(ctx context.Context, request *businesspb.ListBusinessByIdRequest) (*businesspb.ListBusinessByIdResponse, error) {
-	panic("implement me")
+func (s *Server) ListBusinessById(ctx context.Context, req *businesspb.ListBusinessByIdRequest) (*businesspb.ListBusinessByIdResponse, error) {
+	businesses, err := s.repo.ListBusinessByIds(ctx, req.Ids)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Database error")
+	}
+
+	// Convert typedb.Business to proto definition type
+	res := make([]*commonpb.Business, len(businesses))
+	for i, b := range businesses {
+		res[i] = &commonpb.Business{
+			XId:         b.Id.Hex(),
+			Name:        b.BusinessName,
+			Type:        b.Type,
+			Tags:        b.Tags,
+			Description: b.Description,
+			Location: &commonpb.Latlng{
+				Latitude:  b.Location.Coordinates[0],
+				Longitude: b.Location.Coordinates[1],
+			},
+			Address:      b.Address,
+			DisplayImage: b.DisplayImage,
+			Images:       b.Images,
+			Menu:         MenuItemsToProto(b.Menu),
+		}
+	}
+
+	return &businesspb.ListBusinessByIdResponse{Businesses: res}, nil
 }
 
 type Repo interface {
 	ListBusiness(ctx context.Context, searchParams typedb.ListBusinessParams) ([]typedb.Business, error)
 	ListBusinessByIds(ctx context.Context, ids []string) ([]typedb.Business, error)
+}
+
+// Helper function
+func MenuItemsToProto(mi []typedb.MenuItems) []*commonpb.MenuItem {
+	res := make([]*commonpb.MenuItem, len(mi))
+	for i, d := range mi {
+		res[i] = &commonpb.MenuItem{
+			Name:        d.Name,
+			Description: d.Description,
+			Image:       d.Image,
+			Price:       d.Price.String(),
+		}
+	}
+
+	return res
 }
