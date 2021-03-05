bb23ea27216efd1003aef0461cc4a170faad98c9
Traphume <traphumedev@gmail.com>
2021-03-05 15:32:19 +0700

    Implemented AuthenticateAdmin in db pkg

--- internal/database/admindb/admindb.go
+++ internal/database/admindb/admindb.go
@@ -2,7 +2,11 @@ package admindb
 
 import (
 	"context"
+	"github.com/tphume/seatlect-services/internal/commonErr"
+	"github.com/tphume/seatlect-services/internal/database/typedb"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AdminDB struct {
@@ -10,5 +14,23 @@ type AdminDB struct {
 }
 
 func (a *AdminDB) AuthenticateAdmin(ctx context.Context, username string, password string) (string, error) {
-	panic("implement me")
+	res := a.AdminCol.FindOne(ctx, bson.M{"username": username})
+	if res.Err() != nil {
+		if res.Err() == mongo.ErrNoDocuments {
+			return "", commonErr.NOTFOUND
+		}
+
+		return "", commonErr.INTERNAL
+	}
+
+	var admin typedb.Admin
+	if err := res.Decode(&admin); err != nil {
+		return "", commonErr.INTERNAL
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(admin.Password), []byte(password)); err != nil {
+		return "", commonErr.NOTFOUND
+	}
+
+	return admin.Id.Hex(), nil
 }
--- internal/database/typedb/admin.go
+++ internal/database/typedb/admin.go
@@ -1,6 +1,9 @@
 package typedb
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Admin struct {
-	Id       string `bson:"_id"`
-	Username string `bson:"username"`
+	Id       primitive.ObjectID `bson:"_id"`
+	Username string             `bson:"username"`
+	Password string             `bson:"password"`
 }
