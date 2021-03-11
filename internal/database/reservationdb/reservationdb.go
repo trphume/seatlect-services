51894aefea6460392855a65f943bafeb1175990f
Traphume <traphumedev@gmail.com>
2021-03-11 21:42:55 +0700

    Implemented ListReservation in reservationmb

--- internal/database/reservationdb/reservationdb.go
+++ internal/database/reservationdb/reservationdb.go
@@ -2,6 +2,7 @@ package reservationdb
 
 import (
 	"context"
+	"github.com/tphume/seatlect-services/internal/database/typedb"
 	"go.mongodb.org/mongo-driver/mongo"
 	"time"
 )
@@ -10,6 +11,6 @@ type ReservationDB struct {
 	ResCol *mongo.Collection
 }
 
-func (r *ReservationDB) ListReservation(ctx context.Context, id string, start time.Time, end time.Time) {
+func (r *ReservationDB) ListReservation(ctx context.Context, id string, start time.Time, end time.Time) ([]typedb.Reservation, error) {
 	panic("implement me")
 }
--- internal/mobile/reservationmb/reservationmb.go
+++ internal/mobile/reservationmb/reservationmb.go
@@ -2,9 +2,15 @@ package reservationmb
 
 import (
 	"context"
+	"github.com/tphume/seatlect-services/internal/database/typedb"
+	"github.com/tphume/seatlect-services/internal/genproto/commonpb"
 	"github.com/tphume/seatlect-services/internal/genproto/reservationpb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"time"
 )
+
+const iso8601 = "2006-01-02T15:04:05-0700"
 
 type Server struct {
 	Repo Repo
@@ -12,10 +18,38 @@ type Server struct {
 	reservationpb.UnimplementedReservationServiceServer
 }
 
-func (s *Server) ListReservation(ctx context.Context, request *reservationpb.ListReservationRequest) (*reservationpb.ListReservationResponse, error) {
-	panic("implement me")
+func (s *Server) ListReservation(ctx context.Context, req *reservationpb.ListReservationRequest) (*reservationpb.ListReservationResponse, error) {
+	if len(req.Id) <= 0 {
+		return nil, status.Error(codes.Unauthenticated, "ID is not valid")
+	}
+
+	start, err := time.Parse(iso8601, req.Start)
+	if err != nil {
+		start = time.Time{}
+	}
+
+	end, err := time.Parse(iso8601, req.End)
+	if err != nil {
+		end = time.Time{}
+	}
+
+	reservations, err := s.Repo.ListReservation(ctx, req.Id, start, end)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Database error")
+	}
+
+	return &reservationpb.ListReservationResponse{Reservation: typedbToCommonpb(reservations)}, nil
 }
 
 type Repo interface {
-	ListReservation(ctx context.Context, id string, start time.Time, end time.Time)
+	ListReservation(ctx context.Context, id string, start time.Time, end time.Time) ([]typedb.Reservation, error)
+}
+
+// Helper function
+func typedbToCommonpb(resv []typedb.Reservation) []*commonpb.Reservation {
+	panic("implement me")
+}
+
+func seatsToCommonpb(seats []typedb.ReservationSeat) []*commonpb.ReservationSeat {
+	panic("implement me")
 }
