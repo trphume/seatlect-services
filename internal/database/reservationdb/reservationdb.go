a87b775682497a1c392a3eeb3b420c084149e48f
Traphume <traphumedev@gmail.com>
2021-03-13 15:46:36 +0700

    Implemented ListReservation in db pkg

--- internal/database/reservationdb/reservationdb.go
+++ internal/database/reservationdb/reservationdb.go
@@ -2,7 +2,10 @@ package reservationdb
 
 import (
 	"context"
+	"github.com/tphume/seatlect-services/internal/commonErr"
 	"github.com/tphume/seatlect-services/internal/database/typedb"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"time"
 )
@@ -12,5 +15,37 @@ type ReservationDB struct {
 }
 
 func (r *ReservationDB) ListReservation(ctx context.Context, id string, start time.Time, end time.Time) ([]typedb.Reservation, error) {
-	panic("implement me")
+	pId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, commonErr.INVALID
+	}
+
+	var reservations *mongo.Cursor
+	if start.IsZero() || end.IsZero() {
+		reservations, err = r.ResCol.Find(ctx, bson.M{"businessId": pId})
+	} else {
+		reservations, err = r.ResCol.Find(
+			ctx,
+			bson.D{
+				{"businessId", pId},
+				{"start", bson.D{
+					{"$gte", start},
+				}},
+				{"end", bson.D{
+					{"lte", end},
+				}},
+			},
+		)
+	}
+
+	if err != nil {
+		return nil, commonErr.INTERNAL
+	}
+
+	var res []typedb.Reservation
+	if err = reservations.All(ctx, &res); err != nil {
+		return nil, commonErr.INTERNAL
+	}
+
+	return res, nil
 }
