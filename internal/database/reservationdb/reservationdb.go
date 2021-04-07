9ab1273277e782822172f30d671d35f691b4aefb
Traphume <traphumedev@gmail.com>
2021-04-07 23:07:05 +0700

    Implemented ReserveSeats in reservationdb

--- internal/database/reservationdb/reservationdb.go
+++ internal/database/reservationdb/reservationdb.go
@@ -14,6 +14,7 @@ import (
 type ReservationDB struct {
 	ResCol *mongo.Collection
 	BusCol *mongo.Collection
+	OrdCol *mongo.Collection
 }
 
 func (r *ReservationDB) ListReservation(ctx context.Context, id string, start time.Time, end time.Time) ([]typedb.Reservation, error) {
@@ -86,8 +87,96 @@ func (r *ReservationDB) CreateReservation(ctx context.Context, reservation typed
 }
 
 func (r *ReservationDB) ReserveSeats(ctx context.Context, id string, user string, seats []string) (*typedb.Order, error) {
-	// query plan: [match _id = id, match on placement.seats.name = seats[] --> status = EMPTY]
-	panic("implement me")
+	pId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, commonErr.INVALID
+	}
+
+	uId, err := primitive.ObjectIDFromHex(user)
+	if err != nil {
+		return nil, commonErr.INVALID
+	}
+
+	// check seats availability first
+	reservation := r.ResCol.FindOne(ctx, bson.M{"_id": pId})
+	if reservation.Err() != nil {
+		if reservation.Err() == mongo.ErrNoDocuments {
+			return nil, commonErr.NOTFOUND
+		}
+
+		return nil, commonErr.INTERNAL
+	}
+
+	var resv typedb.Reservation
+	if err = reservation.Decode(&resv); err != nil {
+		return nil, commonErr.INTERNAL
+	}
+
+	count := 0
+	typedbSeats := make([]typedb.Seat, len(seats))
+	for _, s := range resv.Placement.Seats {
+		for _, n := range seats {
+			if s.Name == n {
+				typedbSeats[count] = typedb.Seat{
+					Name:     s.Name,
+					Floor:    s.Floor,
+					Type:     s.Type,
+					Space:    s.Space,
+					X:        s.X,
+					Y:        s.Y,
+					Width:    s.Width,
+					Height:   s.Height,
+					Rotation: s.Rotation,
+				}
+				count++
+			}
+		}
+	}
+
+	if count != len(seats) {
+		return nil, commonErr.CONFLICT
+	}
+
+	// update reservation
+	if _, err := r.ResCol.UpdateOne(
+		ctx,
+		bson.M{"_id": pId},
+		bson.D{
+			{
+				"$set",
+				bson.M{
+					"placement.seats.$[elem].user":   uId,
+					"placement.seats.$[elem].status": "TAKEN",
+				},
+			},
+		},
+		options.Update().SetArrayFilters(options.ArrayFilters{Filters: []interface{}{
+			bson.M{"elem.name": bson.M{"$in": seats}},
+		}}),
+	); err != nil {
+		return nil, commonErr.INTERNAL
+	}
+
+	// create order
+	order := typedb.Order{
+		Id:            primitive.NewObjectIDFromTimestamp(time.Now()),
+		ReservationId: pId,
+		CustomerId:    uId,
+		BusinessId:    resv.BusinessId,
+		Start:         resv.Start,
+		End:           resv.End,
+		Seats:         typedbSeats,
+		Status:        "UNUSED",
+		Image:         resv.Image,
+		ExtraSpace:    0,
+		Name:          resv.Name,
+	}
+
+	if _, err := r.OrdCol.InsertOne(ctx, order); err != nil {
+		return nil, commonErr.INTERNAL
+	}
+
+	return &order, nil
 }
 
 // Parsing function
