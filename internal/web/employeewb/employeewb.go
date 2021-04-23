dec78818b3647c3514657bd17c44202ac7520636
Traphume <traphumedev@gmail.com>
2021-04-23 14:14:27 +0700

    Implemented PostEmployeeBusinessId in employeewb

--- internal/web/employeewb/employeewb.go
+++ internal/web/employeewb/employeewb.go
@@ -30,7 +30,29 @@ func (s *Server) GetEmployeeBusinessId(ctx echo.Context, businessId string) erro
 }
 
 func (s *Server) PostEmployeeBusinessId(ctx echo.Context, businessId string) error {
-	panic("implement me")
+	var req employee_api.Employee
+	if err := ctx.Bind(&req); err != nil {
+		return ctx.String(http.StatusBadRequest, "Error binding request body")
+	}
+
+	// Validation
+	if len(req.Username) < 3 || len(req.Password) < 3 {
+		return ctx.String(http.StatusBadRequest, "Username or password is too short")
+	}
+
+	// Call Repo
+	employee := typedb.Employee{Username: req.Username, Password: req.Password}
+	if err := s.Repo.CreateEmployee(ctx.Request().Context(), businessId, employee); err != nil {
+		if err == commonErr.INVALID {
+			return ctx.String(http.StatusBadRequest, "Business id is incorrect")
+		} else if err == commonErr.NOTFOUND {
+			return ctx.String(http.StatusNotFound, "Business not found with given id")
+		}
+
+		return ctx.String(http.StatusInternalServerError, "Database error")
+	}
+
+	return ctx.String(http.StatusCreated, "Employee created successfully")
 }
 
 func (s *Server) DeleteEmployeeBusinessIdUsername(ctx echo.Context, businessId string, username string) error {
